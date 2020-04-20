cb2841b7b71331cf22655ff77107a882b77985a0
RUGMJ7443 <50259227+RUGMJ7443@users.noreply.github.com>
2020-04-20 10:02:40 +0100

    Update fortnite.py

--- fortnite.py
+++ fortnite.py
@@ -505,15 +505,15 @@ async def event_friend_message(message):
 
 
     elif "!ready" in args[0].lower():
-        await client.user.party.me.set_ready(true)
+        await client.user.party.me.set_ready(True)
         await message.reply('Ready!')
 
     elif ("!unready" in args[0].lower()) or ("!sitin" in args[0].lower()):
-        await client.user.party.me.set_ready(false)
+        await client.user.party.me.set_ready(False)
         await message.reply('Unready!')
 
     elif "!sitout" in args[0].lower():
-        await client.user.party.me.set_ready(none)
+        await client.user.party.me.set_ready(None)
         await message.reply('Sitting Out!')
 
     elif "!bp" in args[0].lower():
